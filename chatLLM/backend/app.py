b8a67ac5cea572a987abcc3e63bec13c73b6110b
VorkhlikAS <alevork@gmail.com>
2023-10-25 15:55:28 +0300

    HW4: Changed backend api logic to be consistent with frontend

--- chatLLM/backend/app.py
+++ chatLLM/backend/app.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Store messages in a list (for demonstration purposes).
-messages = []
+# Store dialogues and messages in dictionaries (for demonstration purposes).
+dialogues = {}
+message_id_counter = 1
 
 # Model for a chat message
 class Message:
@@ -11,32 +12,67 @@ class Message:
         self.content = content
         self.likes = 0
 
-# GET request to retrieve messages
-@app.get("/messages")
-def get_messages():
-    return messages
+# Model for a dialogue
+class Dialogue:
+    def __init__(self, name):
+        self.name = name
+        self.messages = []
 
-# POST request to send a message
-@app.post("/send")
-def send_message(content: str):
-    message = Message(content)
-    messages.append(message)
-    return {"message": "Message sent successfully"}
+# GET request to retrieve the list of all user dialogues
+@app.get("/dialogues")
+def get_dialogues():
+    return list(dialogues.values())
 
-# PUT request to like or dislike a message
-@app.put("/like/{message_id}")
-def like_message(message_id: int, like: bool = True):
-    if 0 <= message_id < len(messages):
-        message = messages[message_id]
+# POST request to create a new user dialogue
+@app.post("/create")
+def create_dialogue(dialogue_name: str):
+    dialogue = Dialogue(dialogue_name)
+    dialogues[dialogue_name] = dialogue
+    return {"message": "Dialogue created successfully"}
+
+# PUT request to change the name of a dialogue by its ID
+@app.put("/change/{dialogue_name}")
+def change_dialogue_name(dialogue_name: str, new_name: str):
+    if dialogue_name in dialogues:
+        dialogue = dialogues.pop(dialogue_name)  # Remove the old entry
+        dialogue.name = new_name
+        dialogues[new_name] = dialogue  # Add with the new name as the key
+        return {"message": "Dialogue name changed successfully"}
+    else:
+        return {"error": "Dialogue not found"}
+
+# GET request to retrieve messages in a specific dialogue
+@app.get("/dialogues/{dialogue_name}")
+def get_dialogue_messages(dialogue_name: str):
+    if dialogue_name in dialogues:
+        return dialogues[dialogue_name].messages
+    else:
+        return {"error": "Dialogue not found"}
+
+# POST request to send a message to a specific dialogue
+@app.post("/send/{dialogue_name}")
+def send_message(dialogue_name: str, content: str):
+    if dialogue_name in dialogues:
+        dialogue = dialogues[dialogue_name]
+        message = Message(content)
+        dialogue.messages.append(message)
+        return {"message": "Message sent successfully"}
+    else:
+        return {"error": "Dialogue not found"}
+
+# PUT request to like or dislike a message in a specific dialogue
+@app.put("/like/{dialogue_name}/{message_id}")
+def like_message(dialogue_name: str, message_id: int, like: bool = True):
+    if dialogue_name in dialogues and 0 <= message_id < len(dialogues[dialogue_name].messages):
+        message = dialogues[dialogue_name].messages[message_id]
         if like:
             message.likes += 1
         else:
             message.likes -= 1
         return {"message": "Message liked or disliked successfully"}
     else:
-        return {"error": "Message not found"}
+        return {"error": "Dialogue or message not found"}
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="localhost", port=3000)
-
